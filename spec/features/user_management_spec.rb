1f93c28e8b2a3004a9b9b984cc31944ece5e31e2
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-11-13 12:35:16 +0000

    added test for password conformation

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,8 +1,22 @@
 feature 'user sign up' do
 
+  def sign_up(email: 'rich@gmail.com',
+              password: '12345678',
+              password_confirmation: '12345678')
+    visit '/users/new'
+    fill_in :email, with: email
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
+    click_button 'Sign Up'
+  end
+
   scenario 'I can sign up as a new user' do
     expect { sign_up }.to change(User, :count).by 1
     expect(page).to have_content('Welcome, rich@gmail.com')
     expect(User.first.email).to eq('rich@gmail.com')
+  end
+
+  scenario 'requires a matching confirmation password' do
+    expect { sign_up(password_confirmation :wrong)}.not_to change(User.count)
   end
 end
